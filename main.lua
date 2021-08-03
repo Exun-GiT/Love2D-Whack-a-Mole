b5d6402a6335e1874f2958a68eb30eb5ea577117
Pulkit Garg <pulkitgarg784@gmail.com>
2021-08-03 15:22:15 +0530

    added game timer

--- main.lua
+++ main.lua
@@ -1,20 +1,22 @@
 function love.load()
-  target = {}
-  target.x = 200
-  target.y = 200
-  target.img = love.graphics.newImage("mole.png")
+  moleX = 200
+  moleY = 200
   score = 0
-  timer = 0
+  respawnTimer = 0
+  timer = 10
+  --load files
   scoreFont = love.graphics.newFont("Minecraft.ttf",36)
   popSound = love.audio.newSource("pop.wav", "static")
-  timer = 0
-  won = false
+  moleImg = love.graphics.newImage("mole.png")
+
+  gameover = false
   respawnTime = 0.5
 end
 
 function love.mousepressed(x, y, button)
-  if button == 1 and won == false then
-    if ((x-(target.x+32))^2 + (y-(target.y+32))^2) < 4000 then
+  if button == 1 and gameover == false then
+    dist = math.sqrt((x-(moleX+62.5))^2 + (y-(moleY+45))^2) -- we add half of width and heigh to get center point
+    if dist < 50 then
       score = score+1
       popSound:play()
       setMolePosition()
@@ -22,35 +24,45 @@ function love.mousepressed(x, y, button)
   end
 end
 
+-- this is day 2 task answer
+function love.keypressed( key )
+   if key == "r" and gameover == true then
+	    score = 0
+      gameover = false
+      moleX = 200
+      moleY = 200
+      timer = 10
+   end
+end
+-- day 2 answer endsx
+
 function love.update(dt)
-  if score >= 10 then
-    won = true
-  end
-  if won == false then
-    timer= timer + dt
-    if timer >= respawnTime then
+  if gameover == false then
+    respawnTimer= respawnTimer + dt
+    timer = timer - dt
+    if respawnTimer >= respawnTime then
       setMolePosition()
-      if score>0 then
-        score = score -1
-      end
+    end
+    if timer <= 0 then
+      gameover = true
     end
   end
 end
 
 function setMolePosition()
-  timer = 0
-  target.x = math.random(100 ,love.graphics.getWidth()-100)
-  target.y = math.random(100 ,love.graphics.getHeight()-100)
+  respawnTimer = 0
+  moleX = math.random(100 ,love.graphics.getWidth()-100)
+  moleY = math.random(100 ,love.graphics.getHeight()-100)
 end
 
 function love.draw()
   love.graphics.setBackgroundColor(0.4,0.63,0.93)
   love.graphics.setColor(1, 1, 1)
   love.graphics.setFont(scoreFont)
-  if won == false then
+  if gameover == false then
     love.graphics.print(score,0,0)
   else
-    love.graphics.print("you win",0,0)
+    love.graphics.print("time up. score: ".. score ,0,0)
   end
-  love.graphics.draw(target.img, target.x, target.y)
+  love.graphics.draw(moleImg, moleX, moleY)
 end
